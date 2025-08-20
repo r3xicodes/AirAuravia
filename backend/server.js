0593084aabf15a45d3ea4175801813aea9bc7955
PrinsessaKyti <prinsessakyti@gmail.com>
2025-08-20 15:34:24 -0400

    Added /stats endpoint with mock data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// allow frontend to fetch from backend
+app.use(cors());
+
+// test route
 app.get("/", (req, res) => {
   res.send("Air Auravia API is running...");
+});
+
+// stats route (mock data for now)
+app.get("/stats", (req, res) => {
+  const data = {
+    total_flights: 42,
+    total_hours: 128,
+    total_passengers: 9560,
+  };
+  res.json(data);
 });
 
 app.listen(PORT, () => console.log(`✅ Backend running on port ${PORT}`));
